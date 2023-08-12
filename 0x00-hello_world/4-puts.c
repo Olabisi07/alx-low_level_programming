1cfdbb345d62d48480e90c9b036b44297fa21ebb
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-12 11:17:32 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main- main function
+ *
+ * Retun- return funtion and it ends with 0
+ */
+
 int main() {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
