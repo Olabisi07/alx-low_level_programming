aa43046bf5d788f5d4635532a34cb56bf2075421
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-12 12:35:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description:
+ * This is the main function that serves as the entry point of the program.
+ * It demonstrates the basic structure of a C program and prints a message
+ * to the console.
+ *
+ * Return: indicating successful execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
+
