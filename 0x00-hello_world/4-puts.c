70bae096d17a24c1d4a6047000b9f7c496ad1d3e
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-12 11:28:54 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main- main function
  *
- * Retun- return funtion and it ends with 0
+ * Return: return funtion and it ends with 0
  */
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return (0);
 }
