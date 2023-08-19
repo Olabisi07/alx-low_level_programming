f28e9200b5770c0c4218f3d881ebe4aa4783a2ba
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 06:18:52 -0700

    print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: indicate a successful execution
+*/
+
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 <= 8)
+	{
+		int num2 = num1 + 1;
+
+		while (num2 <= 9)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			if (num1 != 8 || num2 != 9
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
+
