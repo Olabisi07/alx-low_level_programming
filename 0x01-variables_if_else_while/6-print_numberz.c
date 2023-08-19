c6438d37bd30da50eb8276978b2fd32f930567b7
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 02:18:07 -0700

    print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,14 +9,23 @@
 int main(void)
 {
 	putchar('0' + 0);
+	putchar(' ');
 	putchar('0' + 1);
+	putchar(' ');
 	putchar('0' + 2);
+	putchar(' ');
 	putchar('0' + 3);
+	putchar(' ');
 	putchar('0' + 4);
+	putchar(' ');
 	putchar('0' + 5);
+	putchar(' ');
 	putchar('0' + 6);
+	putchar(' ');
 	putchar('0' + 7);
+	putchar(' ');
 	putchar('0' + 8);
+	putchar(' ');
 	putchar('0' + 9);
 
 	putchar('\n');
