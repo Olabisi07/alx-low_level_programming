cb9d75d9a6a44a264facc5bc64cf16cd8f51c7b2
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 05:57:31 -0700

    printcomb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	char ch = '0';
+	int num = '0';
 
-	while (ch <= '9')
+	while (num <= '9')
 
 	{
-		putchar(ch);
-		if (ch != '9')
+		putchar(num + '0');
+		if (num != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		ch++;
+		num++;
 	}
 	putchar('\n');
 	return (0);
