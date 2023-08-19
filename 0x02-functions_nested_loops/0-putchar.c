e26100a660cf1dd673441d12e92f04606a5b2b0b
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 14:39:01 -0700

    putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: indicate a successful execution
+ *
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
