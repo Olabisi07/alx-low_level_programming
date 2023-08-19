f8d5c024e06a83795e51fbebd32ba5eaf92e1f42
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 06:38:34 -0700

    print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: indicate a successful execution
+*/
+
+int main(void)
+{
+	int num1 = 0;
+
+	while (num1 <= 7)
+	{
+		int num2 = num1 + 1;
+
+		while (num2 <= 8)
+		{
+			int num3 = num2 + 1;
+
+			while (num3 <= 9)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+				if (num1 != 7 || num2 != 8 || num3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
+
