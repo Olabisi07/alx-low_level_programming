7f7b91fe077bfc33f424a665c4adb2935f7132a5
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 01:57:45 -0700

    print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: indicate a successful execution
+ *
+*/
+
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
