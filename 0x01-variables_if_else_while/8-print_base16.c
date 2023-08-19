33bce609ceae9eca81ce9bd158ada28c2f3372e7
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-19 05:37:05 -0700

    print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: indicate a successful execution
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
