c987d4e1d0fe1f3146b8a177ce338bb55bf12601
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-20 13:12:44 -0700

    times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,12 @@ void print_times_table(int n)
 	if (n < 0 || n > 15)
 		return;
 
+	int i, j, result;
 	for (int i = 0; i <= n; i++)
 	{
 		for (int j = 0; j <= n; j++)
 		{
-			int result = i * j;
+			result = i * j;
 
 			if (j == 0)
 				printf("%d", result);
