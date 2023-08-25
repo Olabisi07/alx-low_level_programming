8b0f00bf75e37cd656c9d385dc2b385d122ff77e
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-25 06:52:02 -0700

    isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - determine whether a given character is an uppercase letter
+ *
+ * @c: variable text
+ * Return: always 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
