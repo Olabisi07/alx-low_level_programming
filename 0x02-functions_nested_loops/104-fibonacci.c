64eaba126aaf748e56f4bcd7c8c68b2bfd847110
Olabisi07 <horlarbeecy07@gmail.com>
2023-08-26 00:25:32 -0700

    fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,39 +8,21 @@
 
 int main(void)
 {
+	unsigned long int a = 1, b = 2, c;
+	int count;
 
-	unsigned long int i, j, k, j1, j2, k1, k2;
+	printf("%lu, %lu", a, b);
 
-	j = 1;
-	k = 2;
-
-	printf("%lu", j);
-
-	for (i = 1; i < 91; i++)
+	for (count = 2; count < 98; count++)
 	{
-		printf(", %lu", k);
+		c = a + b;
 
-		k = k + j;
-		j = k - j;
-	}
-
-	j1 = j / 1000000000;
-	j2 = j % 1000000000;
-	j1 = k / 1000000000;
-	k2 = k % 1000000000;
-
-	for (i = 92; i < 99; i++)
-	{
-		printf(", %lu", k1 + (k2 / 1000000000));
-		printf("%lu", k2 % 1000000000);
-		k1 = k1 + j1;
-		j1 = k1 - j1;
-		k2 = k2 + j2;
-		j2 = k2 - j2;
+		printf(", %lu", c);
+		a = b;
+		b = c;
 	}
 
 	printf("\n");
-
 	return (0);
 }
 
